883f5404c6e6d217b85145f2a7dfe7fd0116aa82
tstraus <tckrsstrsbgh@gmail.com>
2016-09-10 22:03:02 -0400

    added board and receive moves from the client

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ var http = require('http')
 var fs = require('fs')
 var chalk = require('chalk')
 
+function createArray(length) {
+    var arr = new Array(length || 0), i = length
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1)
+        while (i--) arr[length - 1 - i] = createArray.apply(this.args)
+    }
+
+    return arr
+}
+
+var board = createArray(19, 19)
+
 var index = fs.readFileSync(__dirname + '/index.html')
 
 var app = http.createServer((req, res) => {
@@ -14,12 +27,18 @@ var io = require('socket.io').listen(app)
 io.on('connection', (socket) => {
     console.log(chalk.green('client connected'))
 
-    socket.emit('welcome', { message: 'Welcome!', id: socket.id })
+    //socket.emit('welcome', { message: 'Welcome!', id: socket.id })
 
-    socket.on('client', console.log)
+    socket.on('move', (data) => {
+        console.log(chalk.blue("color: ") + data.color)
+        console.log(chalk.yellow("x: ") + data.x)
+        console.log(chalk.yellow("y: ") + data.y)
 
-    socket.on('message', (data) => {
-        console.log(chalk.blue("move: ") + data.move)
+        if (!(board[data.y][data.x] === 'black') && !(board[data.y][data.x] === 'white')) {
+            board[data.y][data.x] = data.color
+        }
+        
+        console.log(board)
     })
 
     socket.on('disconnect', () => {
